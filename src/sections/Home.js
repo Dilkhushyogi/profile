f2faacd54b99603d6099ee9a23278485a30d169c
Dilkhushyogi <dilkhushyogi2509@gmail.com>
2025-05-27 18:06:35 +0530

    Update Home.js

--- src/sections/Home.js
+++ src/sections/Home.js
@@ -8,6 +8,8 @@ export default function Home() {
         h1 className = "text-4xl font-bold mb-4" > Hi, I 'm Dilkhush Yogi</h1> <
         p className = "mb-4" > Final year BCA student & aspiring developer < /p> <
         a href = "public/Dilkhush Yogi (1).pdf"
+        target="_blank"
+        rel="noopener noreferrer"
         download className = "bg-blue-500 text-white px-6 py-2 rounded-lg hover:bg-blue-600 transition" >
         Download Resume <
         /a> < /
