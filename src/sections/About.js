e86c47eb3d94b8450b085a78a18194048d782448
Dilkhushyogi <dilkhushyogi2509@gmail.com>
2026-06-17 07:37:02 +0530

    Update About.js

--- src/sections/About.js
+++ src/sections/About.js
@@ -6,7 +6,7 @@ export default function About() {
         className = "p-8" >
         <
         h2 className = "text-3xl font-bold mb-4" > About Me < /h2> <
-        p className = "mb-2" > A dedicated and results - driven final - year student with a strong academic background in computer science. < /p> <
+        p className = "mb-2" >Hi, I'm Dilkhush Yogi, a BCA student and aspiring Software Engineer with a strong interest in Python development, automation, and web technologies. I build applications using Python, Django, React, and MySQL, and I also create workflow automations using n8n to streamline business processes and improve productivity. I enjoy solving problems, learning new technologies, and developing practical solutions that have real-world impact. My goal is to build scalable software and automation systems while continuously improving my technical skills. < /p> <
         p className = "mb-1" > Location: Bapu Nagar, Bilwa, Jaipur(302022) < /p> <
         p className = "mb-1" > Email: dilkhushyogi2509 @gmail.com < /p> <
         p className = "mb-1" > Phone: +91 7413992509 < /p> <
